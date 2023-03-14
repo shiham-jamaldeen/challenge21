ddf7cc0adaa0a5d31ed9660e49505ab72bafd75a
shiham-jamaldeen <112249220+shiham-jamaldeen@users.noreply.github.com>
2023-03-15 01:39:05 +1030

    updated connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,12 @@
 const { connect, connection } = require("mongoose");
 
 const connectionString =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks";
-
+  process.env.MONGODB_URI ||
+  "mongodb://127.0.0.1:27017/googlebooks" ||
+  "mongodb+srv://shiham-jamaldeen:<password>@cluster0.cejzrlx.mongodb.net/?retryWrites=true&w=majority";
 connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 module.exports = connection;
